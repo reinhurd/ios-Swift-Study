14effe7f670eccdb3b397ae98650d92201483a57
reinhurd <roarus@yandex.ru>
2021-07-10 23:35:45 +0400

    Add StoryPromt app

---
+++ StoryPrompt/StoryPrompt/ViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  StoryPrompt
+//
+//  Created by Mikhail Bozhenov on 10.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var nounTextField: UITextField!
+    @IBOutlet weak var adjectiveTextField: UITextField!
+    @IBOutlet weak var verbTextField: UITextField!
+    @IBOutlet weak var numberSlider: UISlider!
+    @IBOutlet weak var numberLabel: UILabel!
+    
+    let storyPrompt = StoryPromptEntry();
+    
+    @IBAction func changeNumber(_ sender: UISlider) {
+        numberLabel.text = "Number: \(Int(sender.value))";
+        storyPrompt.number = Int(sender.value);
+    }
+    @IBAction func changeStoryType(_ sender: UISegmentedControl) {
+        if let genre = StoryPrompts.Genre(rawValue: sender.selectedSegmentIndex) {
+            storyPrompt.genre = genre;
+        } else {
+            storyPrompt.genre = .scifi
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        numberSlider.value = 7.5;
+        storyPrompt.noun = "toaster"
+        storyPrompt.adjective = "smelly"
+        storyPrompt.verb = "burps"
+        storyPrompt.number = Int(numberSlider.value)
+    }
+}
